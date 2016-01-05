8abc9a64c288a67935eed7678f8eb19a5e216c79
Yasen Georgiev <avbincco@gmail.com>
2016-01-05 22:23:40 +0200

    Batman commit: BECAUSE IT HAS NOT PARENTS!

---
+++ database/migrations/2016_01_05_200056_CreateUserTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+            $table->enum('type', ['admin', 'teacher', 'student']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
