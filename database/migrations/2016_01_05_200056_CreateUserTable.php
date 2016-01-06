e719942e0b28b007ae7eb9ea50c8b4675e2ac344
Yasen Georgiev <avbincco@gmail.com>
2016-01-06 21:46:41 +0200

    DB schema is updated.

--- database/migrations/2016_01_05_200056_CreateUserTable.php
+++ database/migrations/2016_01_05_200056_CreateUserTable.php
@@ -18,8 +18,54 @@ class CreateUserTable extends Migration
             $table->string('password');
             $table->string('first_name', 20);
             $table->string('last_name', 20);
+            $table->integer('class_id')
+                ->unsigned()
+                ->nullable();
             $table->enum('type', ['admin', 'teacher', 'student']);
             $table->timestamps();
+        });
+        Schema::create('subjects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('profiles', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('classes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('profile_id')
+                ->unsigned();
+            $table->foreign('profile_id')
+                ->references('id')->on('profiles');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('class_subjects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('class_id')
+                ->unsigned();
+            $table->foreign('class_id')
+                ->references('id')->on('classes');
+            $table->integer('subject_id')
+                ->unsigned();
+            $table->foreign('subject_id')
+                ->references('id')->on('subjects');
+            $table->integer('teacher_id')
+                ->unsigned();
+            $table->foreign('teacher_id')
+                ->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        // @todo students; method of study; marks type; relation between student, subject and mark
+
+
+        Schema::table('users', function(Blueprint $table) {
+            $table->foreign('class_id')
+                ->references('id')->on('classes');
         });
     }
 
@@ -30,6 +76,9 @@ class CreateUserTable extends Migration
      */
     public function down()
     {
+        Schema::drop('profile_subjects');
+        Schema::drop('profiles');
+        Schema::drop('subjects');
         Schema::drop('users');
     }
 }
